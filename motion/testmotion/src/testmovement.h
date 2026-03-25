6e05d8066cb66885eebc129da364b76e89112605
Grant Taylor <grant.i.taylor@student.uts.edu.au>
2026-03-25 12:43:30 +1100

    adding motion workspace

---
+++ motion/testmotion/src/testmovement.h
@@ -0,0 +1,7 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+
